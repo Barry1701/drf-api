96b585e6c712c9bfc0028cf90ebda148ce7a217a
Bartosz Stanczuk <synergy170185@gmail.com>
2024-09-26 15:16:52 +0000

    change debug for dev in os.environ

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,7 +60,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['8000-barry1701-drfapi-sa8d0qg8z40.ws-eu116.gitpod.io',
 'rest-framework-project-d7776b473078.herokuapp.com' 
